e37fdfc824df65212af6186fc24b0d970c65a696
anuvarov <anuvarov0103@gmail.com>
2021-06-29 10:54:51 +0500

    My third day, third project

---
+++ Day-3-Rotating-Navigation/script.js
@@ -0,0 +1,9 @@
+const open = document.getElementById('open'),
+    close = document.getElementById('close'),
+    container = document.querySelector('.container');
+
+open.addEventListener('click', () => container.classList.add('show-nav'));
+
+close.addEventListener('click', () => container.classList.remove('show-nav'));
+
+console.log(container)
